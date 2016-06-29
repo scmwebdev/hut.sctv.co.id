5924b067ef7f21d7354c02308961e9040c72727a
gamal <gamal.pratama@scm.co.id>
2016-06-29 16:56:55 +0700

    Underscore Theme (DEV)
    - rename hut.sctv.co.id to underscore theme
    - update gulpfile.js with gulp-load-plugins for dynamic calling
    - add paths variable in guplfilejs for easy maintenances
    - massive overhaul on sass folder

---
+++ wp-content/themes/underscore/page-personality-quiz.php
@@ -0,0 +1,18 @@
+<?php 
+	
+	/**
+	 * Template for Personality Quiz Page
+	 * @package HUT SCTV
+	 */
+
+	get_header();
+?>
+
+	<main class="quiz" id="personality-quiz">
+		<div class="container">
+			<h1 class="text-center">Personality Quiz</h1>
+		</div>
+	</main>
+
+
+<?php get_footer(); ?>
