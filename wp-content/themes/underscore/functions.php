1d777cf3ccc4f1fefa4b6c4f2641afe874c252f8
gamal <gamal.pratama@scm.co.id>
2016-06-30 08:59:02 +0700

    remove default js and use the compiled one (main.js)

--- wp-content/themes/underscore/functions.php
+++ wp-content/themes/underscore/functions.php
@@ -103,10 +103,8 @@ add_action( 'widgets_init', 'hut_sctv_underscore_widgets_init' );
  */
 function hut_sctv_underscore_scripts() {
 	wp_enqueue_style( 'hut-sctv-underscore-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'hut-sctv-underscore-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'hut-sctv-underscore-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	
+	wp_enqueue_script( 'main', get_template_directory_uri() . '/main.js', array(), true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
