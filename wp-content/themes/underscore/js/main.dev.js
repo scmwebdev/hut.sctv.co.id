1793eb1ae2e64911fd80ac5c2904d2e3cf839283
gamal <gamal.pratama@scm.co.id>
2016-06-30 17:11:46 +0700

    score WIP

--- wp-content/themes/underscore/js/main.dev.js
+++ wp-content/themes/underscore/js/main.dev.js
@@ -4,10 +4,24 @@
 
 var PersonalityQuiz = {
 
-    start: function() {
+    startQuiz: function(settings) {
+        PersonalityQuiz.config = {
+            jsonURL: siteUrl + '/wp-content/themes/underscore/json/personality.json',
+        }
+        $.extend(PersonalityQuiz.config, settings);
 
-        var jsonURL = siteUrl + '/wp-content/themes/underscore/json/personality.json';
-        
+        // grab the json file
+        PersonalityQuiz.getData();
+
+        // execute bindUI() once the ajax is complete
+        $(document).ajaxComplete(function() {
+            PersonalityQuiz.bindUI();
+        });
+    },
+    getData: function() {
+
+        var jsonURL = PersonalityQuiz.config.jsonURL;
+
 
         $.getJSON(jsonURL, { format: "json" })
             .fail(function(jqxhr, textStatus, error) {
@@ -16,7 +30,7 @@ var PersonalityQuiz = {
             })
             .done(function(data) {
 
-            	var personaArr = [];
+                var personaArr = [];
                 for (i = 0; i < data.length; i++) {
                     personaArr.push('<div class="item" data-score="' + data[i].score + '"><img class="img-responsive full-width" src="' + data[i].img + '"></div>');
                 }
@@ -28,10 +42,10 @@ var PersonalityQuiz = {
     },
     displayData: function() {
 
-        var item = $('.quiz-container > .item');
+        var itemPersona = $('.quiz-container > .item');
         var itemContainer = $('.quiz-container').width();
-        var totalItem = item.length;
-        $(item).css('width', function() {
+        var totalItem = itemPersona.length;
+        $(itemPersona).css('width', function() {
             return itemContainer / totalItem + 'px';
         })
 
@@ -43,18 +57,60 @@ var PersonalityQuiz = {
             "class": "quiz-container clearfix spacepad-15",
             html: targetArray.join("")
         }).appendTo(container);
+
         $('<div>', {
             'id': 'info'
         }).appendTo(container);
 
-    }
+        $('<div>', {
+            'id': 'score'
+        }).appendTo(container);
+
+    },
+    bindUI: function() {
+        var score = 0;
+        var item = $('.quiz-container > .item');
+        var displayScore = $('#score').val(score);
+
+        item.click(function() {
+            // var getScore = $(this).attr('data-score');
+            $(this).addClass('selected');
+            score++;
+        });
+
+
+        // PersonalityQuiz.getScore();
+    },
+    // getScore: function() {
+
+    //     var scoreBtn = $('#btn-score');
+    //     var itemSelected = $('.quiz-container > .item.selected');
+    //     var itemSelectedAttr = itemSelected.attr('data-score');
+    //     var item = $('.quiz-container > .item');
+    //     var itemAttr = item.attr('data-score');
+
+
+    //     scoreBtn.on('click', function(evt) {
+    //         evt.stopPropagation();
+    //         console.log($(itemSelected).length);
+
+    //     });
+
+    // console.log(itemSelected.attr('data-score'));
+// }
 
 };
 
 (function($) {
 
     console.log('forbidden lover');
-	PersonalityQuiz.start();
+    PersonalityQuiz.startQuiz();
+
+    var scoreBtn = $('#btn-score');
+    var itemSelected = $('.quiz-container > .item.selected');
+    var itemSelectedAttr = itemSelected.attr('data-score');
+    var item = $('.quiz-container > .item');
+    var itemAttr = item.attr('data-score');
 
 
 })(jQuery);
--- wp-content/themes/underscore/page-personality-quiz.php
+++ wp-content/themes/underscore/page-personality-quiz.php
@@ -11,6 +11,9 @@
 	<main class="quiz" id="personality-quiz">
 		<div class="container">
 			<h1 class="text-center"><?php the_title(); ?></h1>
+			<div>
+			<button type="button" class="btn btn-primary" id="btn-score">Get Score</button>
+			</div>
 		</div>
 	</main>
 
