8f71381e3c25ac2bd57ccb7cbf26d5e3dcd885b0
gamal <gamal.pratama@scm.co.id>
2016-06-30 08:59:42 +0700

    add an array of js files and list of files to move when gulp dist is running

--- wp-content/themes/underscore/gulpfile.js
+++ wp-content/themes/underscore/gulpfile.js
@@ -8,6 +8,7 @@ var reload = browserSync.reload;
  * Paths Variable
  * ======================================================== */
 
+// base paths for the project
 var basePaths = {
     root: '../',
     theme: './',
@@ -16,18 +17,37 @@ var basePaths = {
     npm: './node_modules/'
 }
 
+// paths for the css/scss
 var style = {
     main: basePaths.style + 'style.scss',
     vendor: basePaths.style + 'vendor/'
 }
 
+// paths for npm modules
 var module = {
     slick: basePaths.npm + 'slick-carousel/slick/',
     fontawesome: basePaths.npm + 'slick-carousel/font-awesome/',
     bootstrap: basePaths.npm + 'bootstrap-plugins.sass/assets/'
 }
 
+// paths for the dist folder
+var dist = './dist';
 
+// list of files that are being moved to dist folder
+var filesToMove = [
+    '*.php',
+    '*.css',
+    './template-parts/*',
+    './inc/*',
+    './fonts/*'
+];
+
+var jsFiles = [
+    basePaths.npm + 'jquery/dist/jquery.js',
+    basePaths.npm + 'fastclick/lib/*.js',
+    module.slick + 'slick.js',
+    'js/main.js'
+]
 /* ========================================================
  * Tasks with Browser Sync
  * ======================================================== */
@@ -48,33 +68,24 @@ gulp.task('browserSync', function() {
 gulp.task('style', function() {
     return gulp.src(style.main)
         .pipe(plugins.plumber({
-            errorHandler: function (err) {
+            errorHandler: function(err) {
                 console.log(err);
                 this.emit('end');
             }
         }))
         .pipe(plugins.sourcemaps.init())
         .pipe(plugins.sass())
-        // .pipe(plugins.sass({ outputStyle: 'compressed' }))
         .pipe(plugins.autoprefixer())
         .pipe(plugins.sourcemaps.write())
         .pipe(plugins.rename('style.css'))
-        .pipe(gulp.dest(basePaths.theme)) 
+        .pipe(gulp.dest(basePaths.theme))
         .pipe(reload({ stream: true }));
 });
 
 gulp.task('js', function() {
-    return gulp.src([
-            './node_modules/jquery/dist/jquery.js',
-            './node_modules/fastclick/lib/*.js',
-            './node_modules/slick-carousel/slick/slick.js',
-            path + '/js/news-sctv.js',
-        ])
-        .pipe(plugins.concat('news-sctv.js'))
-        .pipe(gulp.dest(path))
-        .pipe(plugins.uglify())
-        .pipe(plugins.concat('news-sctv.min.js'))
-        .pipe(gulp.dest(basePaths))
+    return gulp.src(jsFiles)
+        .pipe(plugins.concat('main.js'))
+        .pipe(gulp.dest(basePaths.theme))
         .pipe(reload({ stream: true }));
 });
 
@@ -83,15 +94,15 @@ gulp.task('js', function() {
  * Utility Tasks
  * ======================================================== */
 
-gulp.task('bootstrap', function(){
+gulp.task('bootstrap', function() {
 
     var bootstrapStyle = gulp.src(module.bootstrap + 'stylesheets/**/**/*')
         .pipe(gulp.dest(style.vendor + 'bootstrap-plugins.sass'));
-    
+
     return merge(bootstrapStyle);
 });
 
-gulp.task('font-awesome', function(){
+gulp.task('font-awesome', function() {
 
     //move font awesome scss to our main plugins.sass folder
     var faStyle = gulp.src(module.fontawesome + 'scss/*')
@@ -100,23 +111,23 @@ gulp.task('font-awesome', function(){
     //move font_awesome fonts to themes root folder
     var faFont = gulp.src(module.fontawesome + 'fonts/**/*')
         .pipe(gulp.dest(basePaths.root + 'fonts'));
- 
+
     //merge tasks
     return merge(faStyle, faFont);
 
 });
 
-gulp.task('slicky', function(){
+gulp.task('slicky', function() {
 
     // move slick fonts to the fonts under our themes folder
     var slickFont = gulp.src(module.slick + 'fonts/*')
-        .pipe(gulp.dest( './fonts'));
+        .pipe(gulp.dest('./fonts'));
 
     var slickStyle = gulp.src([
-        module.slick  + 'slick.scss',
-        module.slick  + 'slick-theme.scss'
+            module.slick + 'slick.scss',
+            module.slick + 'slick-theme.scss'
         ])
-    .pipe(gulp.dest(style.vendor + 'slick'));
+        .pipe(gulp.dest(style.vendor + 'slick'));
 
     // move ajax loader to custom themes folder
     var slickAjaxLoader = gulp.src(module.slick + 'ajax-loader.gif')
@@ -134,7 +145,42 @@ gulp.task('utility', ['bootstrap', 'slicky', 'font-awesome']);
  * ======================================================== */
 
 gulp.task('default', ['style', 'js', 'browserSync'], function() {
-    gulp.watch('*.scss', {cwd: path + '/sass'}, ['style']);
-    gulp.watch('**/*.scss', {cwd: path + '/sass'}, ['style']);
-    gulp.watch('*.js', {cwd: path + '/js'}, ['js']);
+    gulp.watch('*.scss', { cwd: path + '/sass' }, ['style']);
+    gulp.watch('**/*.scss', { cwd: path + '/sass' }, ['style']);
+    gulp.watch('*.js', { cwd: path + '/js' }, ['js']);
 });
+
+/* ========================================================
+ * Production Tasks
+ * ======================================================== */
+
+gulp.task('prod-style', function() {
+    return gulp.src(style.main)
+        .pipe(plugins.plumber({
+            errorHandler: function(err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sass({ outputStyle: 'compressed' }))
+        .pipe(plugins.autoprefixer())
+        .pipe(plugins.sourcemaps.write())
+        .pipe(plugins.rename('style.css'))
+        .pipe(gulp.dest(dist))
+        .pipe(reload({ stream: true }));
+});
+
+gulp.task('prod-js', function() {
+    return gulp.src(jsFiles)
+        .pipe(plugins.concat('main.min.js'))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest(dist))
+});
+
+gulp.task('setup', function() {
+    return gulp.src(filesToMove, { base: './' })
+        .pipe(gulp.dest(dist))
+})
+
+gulp.task('dist', ['prod-style', 'prod-js', 'setup']);
