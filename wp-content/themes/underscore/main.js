889dd83bd282a199c6b407aef20902d8d70b697b
gamal <gamal.pratama@scm.co.id>
2016-06-30 14:48:55 +0700

    update quiz js with oop style + add mobile and desktop min-width for each item

--- wp-content/themes/underscore/main.js
+++ wp-content/themes/underscore/main.js
@@ -12933,37 +12933,59 @@ return jQuery;
  * Main JS
  */
 
+var PersonalityQuiz = {
+
+    start: function() {
+
+        var jsonURL = siteUrl + '/wp-content/themes/underscore/json/personality.json';
+        
+
+        $.getJSON(jsonURL, { format: "json" })
+            .fail(function(jqxhr, textStatus, error) {
+                var err = textStatus + ", " + error;
+                $('#info').html(err);
+            })
+            .done(function(data) {
+
+            	var personaArr = [];
+                for (i = 0; i < data.length; i++) {
+                    personaArr.push('<div class="item" data-score="' + data[i].score + '"><img class="img-responsive full-width" src="' + data[i].img + '"></div>');
+                }
+
+                PersonalityQuiz.appendUI(personaArr);
+                PersonalityQuiz.displayData();
+
+            })
+    },
+    displayData: function() {
+
+        var item = $('.quiz-container > .item');
+        var itemContainer = $('.quiz-container').width();
+        var totalItem = item.length;
+        $(item).css('width', function() {
+            return itemContainer / totalItem + 'px';
+        })
+
+    },
+    appendUI: function(targetArray) {
+
+        var container = $('#personality-quiz .container');
+        $("<div>", {
+            "class": "quiz-container clearfix spacepad-15",
+            html: targetArray.join("")
+        }).appendTo(container);
+        $('<div>', {
+            'id': 'info'
+        }).appendTo(container);
+
+    }
+
+};
+
 (function($) {
 
     console.log('forbidden lover');
-
-    var jsonURL = siteUrl + '/wp-content/themes/underscore/json/personality.json';
-
-    $.getJSON(jsonURL, {
-            format: "json"
-        })
-        .fail(function(jqxhr, textStatus, error) {
-            var err = textStatus + ", " + error;
-            // console.log("Request Failed: " + err);
-            $('#info').html(err);
-        })
-        .done(function(data) {
-
-        	var arr =[];
-            for (i = 0; i < data.length; i++) {
-                arr.push('<div class="item" data-score="'+ data[i].score +'"><img src="' + data[i].img + '"></div>');
-            }
-
-            var container = $('#personality-quiz .container');
-            $("<div>", {
-                "class": "quiz-container clearfix spacepad-15",
-                html: arr.join("")
-            }).appendTo(container);
-            $('<div>', {
-            	'id': 'info'
-            }).appendTo(container);
-        })
+	PersonalityQuiz.start();
 
 
 })(jQuery);
-
