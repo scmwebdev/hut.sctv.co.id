bcd87fcc6a0df462fe9d035b799f0cfdab489e2f
gamal <gamal.pratama@scm.co.id>
2016-07-22 16:11:44 +0700

    Frontpage - Latest Video
    - hide arrows on mobile
    - add margin 10em instead of padding 10em
    - update carousel settings on mobile & desktop

--- wp-content/themes/underscore/main.js
+++ wp-content/themes/underscore/main.js
@@ -13605,19 +13605,30 @@ var page = (function() {
     function vidio_latest() {
         var vidio = $('#video-on-demand > div');
         vidio.slick({
-        	slidesToShow: 3,
-        	slidesToScroll: 1,
-        	nextArrow: '<div class="custom-slick-next slick-arrow"><i class="fa fa-chevron-circle-right"></i></div>',
-        	prevArrow: '<div class="custom-slick-prev slick-arrow"><i class="fa fa-chevron-circle-left"></i></div>'
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            draggable: false,
+            nextArrow: '<div class="custom-slick-next slick-arrow"><i class="fa fa-chevron-circle-right"></i></div>',
+            prevArrow: '<div class="custom-slick-prev slick-arrow"><i class="fa fa-chevron-circle-left"></i></div>',
+            responsive: [{
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    draggable: true
+                }
+            }]
         });
     }
 
     return {
-    	vidio_latest:vidio_latest
+        vidio_latest: vidio_latest
     }
 
 }());
 
 $(document).ready(function() {
-	page.vidio_latest();
+    page.vidio_latest();
 });
