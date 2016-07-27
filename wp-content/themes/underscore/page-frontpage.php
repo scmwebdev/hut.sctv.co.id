27febcc1f6a61e9681d840e5e6221920c4eba2db
gamal <gamal.pratama@scm.co.id>
2016-07-27 17:58:06 +0700

    create a few new scss to handle post-lists, sponsor lists, and slick carousel overrides

--- wp-content/themes/underscore/page-frontpage.php
+++ wp-content/themes/underscore/page-frontpage.php
@@ -40,7 +40,16 @@
 			</div>
 
 		</section>
-		<section class="" id="sponsor">
+		<section class="clearfix" id="sponsor">
+			<div class="container spacepad-15">
+				<img class="img-responsive center-block" src="<?php echo get_template_directory_uri() . '/asset/our_sponsor.png' ?>" alt="our sponsor">
+				<div class="slick-container">
+					<?php get_sponsor() ?>
+				</div>
+				
+			</div>
+			
+			
 		</section>
 	</div>
 
