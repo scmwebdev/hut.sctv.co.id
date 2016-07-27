f8dd1a9ab28222c59d2251bcf8c8c3b79cdf71a9
gamal <gamal.pratama@scm.co.id>
2016-07-27 17:55:35 +0700

    set the sponsor post lists with slick carousel

--- wp-content/themes/underscore/js/main.dev.js
+++ wp-content/themes/underscore/js/main.dev.js
@@ -2,6 +2,9 @@ var page = (function() {
 
     console.log('Hontou no Oto');
 
+    /**
+     * set the slick carousel settings for the latest video
+     */
     function vidio_latest() {
         var vidio = $('#video-on-demand > div');
         vidio.slick({
@@ -21,16 +24,39 @@ var page = (function() {
                 }
             }]
         });
-    }
+    };
+
+
+    function sponsor_list() {
+        $('#sponsor .slick-container').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            // autoplay: true,
+            draggable: false,
+            nextArrow: '<div class="custom-slick-next slick-arrow"><i class="fa fa-chevron-right"></i></div>',
+            prevArrow: '<div class="custom-slick-prev slick-arrow"><i class="fa fa-chevron-left"></i></div>',
+            responsive: [{
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    draggable: true
+                }
+            }]
+        });
+    };
 
     return {
-        vidio_latest: vidio_latest
+        vidio_latest: vidio_latest,
+        sponsor_list:sponsor_list
     }
 
 }());
 
 $(document).ready(function() {
     page.vidio_latest();
+    page.sponsor_list();
 
     /** add arrow DOM on the menu when hovered */
     var parentMenu = $('.desktop #primary-menu > li > a');
@@ -38,5 +64,8 @@ $(document).ready(function() {
 
     PageHeader.letThereBeDOM(parentMenu, icon);
     // **
+
+
+
 
 });
--- wp-content/themes/underscore/main.js
+++ wp-content/themes/underscore/main.js
@@ -13666,6 +13666,9 @@ var page = (function() {
 
     console.log('Hontou no Oto');
 
+    /**
+     * set the slick carousel settings for the latest video
+     */
     function vidio_latest() {
         var vidio = $('#video-on-demand > div');
         vidio.slick({
@@ -13685,16 +13688,39 @@ var page = (function() {
                 }
             }]
         });
-    }
+    };
+
+
+    function sponsor_list() {
+        $('#sponsor .slick-container').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            // autoplay: true,
+            draggable: false,
+            nextArrow: '<div class="custom-slick-next slick-arrow"><i class="fa fa-chevron-right"></i></div>',
+            prevArrow: '<div class="custom-slick-prev slick-arrow"><i class="fa fa-chevron-left"></i></div>',
+            responsive: [{
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    draggable: true
+                }
+            }]
+        });
+    };
 
     return {
-        vidio_latest: vidio_latest
+        vidio_latest: vidio_latest,
+        sponsor_list:sponsor_list
     }
 
 }());
 
 $(document).ready(function() {
     page.vidio_latest();
+    page.sponsor_list();
 
     /** add arrow DOM on the menu when hovered */
     var parentMenu = $('.desktop #primary-menu > li > a');
@@ -13702,5 +13728,8 @@ $(document).ready(function() {
 
     PageHeader.letThereBeDOM(parentMenu, icon);
     // **
+
+
+
 
 });
